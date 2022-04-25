0b961b466a22fdbdab7508f4536a0a1845f23e67
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-04-24 20:54:04 -0700

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -48,8 +48,7 @@ set_bg('assets/background.png')
 # User input text request
 input_sent = st.text_area("Input Text", "", height=200)
 # User input for summarization percent request
- ratio =  st.slider(
- "Summarization factor", min_value=0.0, max_value=1.0, value=0.3, step=0.01 
+ratio =  st.slider("Summarization factor", min_value=0.0, max_value=1.0, value=0.3, step=0.01 
 )
 summarized_text = summarizer.summarize(
 input_sent, ratio=ratio, language="english", split=True, scores=True
