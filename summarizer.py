4e71c6190e3a78a85e69598a54c37634b4643cf9
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-12-28 17:33:41 -0800

    Update

--- summarizer.py
+++ summarizer.py
@@ -149,7 +149,12 @@ if search:
 
     # create a map centered at the average latitude and longitude of the restaurants
     map = folium.Map(location=[lat, lng], zoom_start=13)
-    # st_folium(map, width=500)
+    st_map = st_folium(map, width=700, height=450)
+
+    state_name = ''
+    if st_map['last_active_drawing']:
+        state_name = st_map['last_active_drawing']['properties']['name']
+    state_name
     # folium.Marker( location=[lat, lng], icon=folium.Icon(color='red') , popup="Current Location").add_to(map)
 
     # def get_color(value):
