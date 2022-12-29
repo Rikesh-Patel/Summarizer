f7eed11f9d8700ff6970a398fee79cca0d00f6fd
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-12-28 18:14:31 -0800

    update

--- summarizer.py
+++ summarizer.py
@@ -144,43 +144,43 @@ if search:
 
    
 
-    # create a map centered at the average latitude and longitude of the restaurants
-    map = folium.Map(location=[lat, lng], zoom_start=13,  scrollWheelZoom=False)
-    
+# create a map centered at the average latitude and longitude of the restaurants
+map = folium.Map(location=[lat, lng], zoom_start=13,  scrollWheelZoom=False)
 
 
-    # folium.Marker( location=[lat, lng], icon=folium.Icon(color='red') , popup="Current Location").add_to(map)
 
-    # def get_color(value):
-    #     # Map the value to a color scale from yellow to green
-    #     value = (value - 1.0) / (5.0 - 1.0)
-    #     r = int(255 * (1 - value))
-    #     g = int(255 * value)
-    #     b = 0
-    #     return f'#{r:02x}{g:02x}{b:02x}'
+# folium.Marker( location=[lat, lng], icon=folium.Icon(color='red') , popup="Current Location").add_to(map)
 
-    # # add a marker for each restaurant
-    # def create_marker(row):
-    #     # Create a marker at the latitude and longitude specified in the row
-    #     marker = folium.Marker( location=[row['coordinates.latitude'], 
-    #                                     row['coordinates.longitude']], 
-    #                         popup=f"<a href={row['url']}>{row['name']}</a>",
-    #                         icon=folium.DivIcon(
-    #                                 icon_size=(36,36),
-    #                                 icon_anchor=(18,36),
-    #                                 html='<div style="display: flex; align-items: center;">'
-    #                                     f'<i class="fa fa-map-marker" style="font-size: 30pt;text-shadow: 2px 1px black; color: {get_color(row["rating"])}"></i>'
-                                        
-    #                                     f'<div style="font-size: 12pt; font-weight: bold; color: white;text-align:left; margin-left: -22px;text-shadow: 2px 1px black;" > {row["rating"]}</div>'
-    #                                     f'<div style="font-size: 8pt; font-weight: bold; line-height: 1; margin-left: 8px; text-shadow: 2px 1px white;">{row["name"]}</div>'
-    #                                     '</div>'
-            
-    #     ))
-    #     return marker.add_to(map)
+# def get_color(value):
+#     # Map the value to a color scale from yellow to green
+#     value = (value - 1.0) / (5.0 - 1.0)
+#     r = int(255 * (1 - value))
+#     g = int(255 * value)
+#     b = 0
+#     return f'#{r:02x}{g:02x}{b:02x}'
 
-    # # display the map
-    # df.apply(create_marker, axis=1)
-    # st_folium(map)
+# # add a marker for each restaurant
+# def create_marker(row):
+#     # Create a marker at the latitude and longitude specified in the row
+#     marker = folium.Marker( location=[row['coordinates.latitude'], 
+#                                     row['coordinates.longitude']], 
+#                         popup=f"<a href={row['url']}>{row['name']}</a>",
+#                         icon=folium.DivIcon(
+#                                 icon_size=(36,36),
+#                                 icon_anchor=(18,36),
+#                                 html='<div style="display: flex; align-items: center;">'
+#                                     f'<i class="fa fa-map-marker" style="font-size: 30pt;text-shadow: 2px 1px black; color: {get_color(row["rating"])}"></i>'
+                                    
+#                                     f'<div style="font-size: 12pt; font-weight: bold; color: white;text-align:left; margin-left: -22px;text-shadow: 2px 1px black;" > {row["rating"]}</div>'
+#                                     f'<div style="font-size: 8pt; font-weight: bold; line-height: 1; margin-left: 8px; text-shadow: 2px 1px white;">{row["name"]}</div>'
+#                                     '</div>'
+        
+#     ))
+#     return marker.add_to(map)
+
+# # display the map
+# df.apply(create_marker, axis=1)
+# st_folium(map)
 
 st_map = st_folium(map, width=700, height=450)
     
