18de370aa41b9d90dc0732b143a5731cf2b324b0
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2022-12-28 18:04:25 -0800

    update

--- summarizer.py
+++ summarizer.py
@@ -95,102 +95,15 @@ if selected_option:
     st.write(f'Latitude: {lat}')
     st.write(f'Longitude: {lng}')
     search = st.button("Search")
+    # Map
 
-import folium
-from streamlit_folium import st_folium
+    import folium
+    from streamlit_folium import st_folium
 
 
     # create a map centered at the average latitude and longitude of the restaurants
-    
-if search:
     map = folium.Map(location=[lat, lng])
     st_map = st_folium(map, width=700, height=450)
-    
-if search:
-    # Yelp
-    # Get Business ID
-    import requests
-    #&latitude=latitude&longitude=longtiude&radius=radius
-    query = "Pizza"
-    location = "culver city, ca"
-    url = f"https://api.yelp.com/v3/businesses/search?latitude={lat}&longitude={lng}&term={query}&categories=&sort_by=best_match&limit=20"
-
-    headers = {
-        "accept": "application/json",
-        "Authorization": "Bearer A_1nx-eEZxP4IQ-fT7r32jAHjBIU1gQqNzMM5hkc-XGtQFSbeRJr5FNXyXxVBsEA7z5r47W_7rGMK6-hc2OkoUJE_bpgtZ4Oq2zndySrIjBCvS0kH2EWcmxSyx2fY3Yx"
-    }
-
-    response = requests.get(url, headers=headers)
-    if 'error' in response.json():
-        print(response.json()['error']['code'])
-    
-    import json
-    import pandas as pd
-    df = pd.json_normalize(response.json(), 'businesses')
-    df = df.sort_values("distance")
-
-    def extract_list(json_obj):
-        # flat_df = pd.json_normalize(json_obj)
-        # return flat_df['name'].tolist()
-        return [json['title'] for json in json_obj]
-
-    # Apply the function to each row in the DataFrame
-    df['categories'] = df['categories'].apply(extract_list)
-    df = df.loc[:,df.columns.isin(['id', 'name', 'image_url', 'is_closed', 'url', 'review_count',
-        'categories', 'rating', 'transactions', 'price', 'display_phone',
-        'distance', 'coordinates.latitude', 'coordinates.longitude',
-        'location.display_address'])]
-    # df = df[df['is_closed']=="false"]
-    df = df.loc[:,df.columns.isin(['name', 'url', 'review_count',
-        'categories', 'rating', 'transactions', 'price', 'display_phone',
-        'distance','location.display_address'])]
-       
-    
-    # Allow the user to sort the data based on any column
-    sort_column = st.selectbox('Sort by column', df.columns)
-    
-    st.dataframe(df.sort_values(by=sort_column)) 
-
-
-    
-
-
-    # folium.Marker( location=[lat, lng], icon=folium.Icon(color='red') , popup="Current Location").add_to(map)
-
-    # def get_color(value):
-    #     # Map the value to a color scale from yellow to green
-    #     value = (value - 1.0) / (5.0 - 1.0)
-    #     r = int(255 * (1 - value))
-    #     g = int(255 * value)
-    #     b = 0
-    #     return f'#{r:02x}{g:02x}{b:02x}'
-
-    # # add a marker for each restaurant
-    # def create_marker(row):
-    #     # Create a marker at the latitude and longitude specified in the row
-    #     marker = folium.Marker( location=[row['coordinates.latitude'], 
-    #                                     row['coordinates.longitude']], 
-    #                         popup=f"<a href={row['url']}>{row['name']}</a>",
-    #                         icon=folium.DivIcon(
-    #                                 icon_size=(36,36),
-    #                                 icon_anchor=(18,36),
-    #                                 html='<div style="display: flex; align-items: center;">'
-    #                                     f'<i class="fa fa-map-marker" style="font-size: 30pt;text-shadow: 2px 1px black; color: {get_color(row["rating"])}"></i>'
-                                        
-    #                                     f'<div style="font-size: 12pt; font-weight: bold; color: white;text-align:left; margin-left: -22px;text-shadow: 2px 1px black;" > {row["rating"]}</div>'
-    #                                     f'<div style="font-size: 8pt; font-weight: bold; line-height: 1; margin-left: 8px; text-shadow: 2px 1px white;">{row["name"]}</div>'
-    #                                     '</div>'
-            
-    #     ))
-    #     return marker.add_to(map)
-
-    # # display the map
-    # df.apply(create_marker, axis=1)
-    # st_folium(map)
-
-    # Map
-
-
     
 
 # User input text request
