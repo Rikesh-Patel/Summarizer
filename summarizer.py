ecd3a8cefec22a44cea2a1969b376b4790f9e992
Rikesh-Patel <103740388+Rikesh-Patel@users.noreply.github.com>
2023-01-15 12:16:42 -0800

    update

--- summarizer.py
+++ summarizer.py
@@ -305,7 +305,7 @@ if st.session_state['button'] == True:
                 # if sentiment == 'neutral':
                 #     return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
                 # if sentiment == 'negative':
-                    return "hsl(%d%, 100%%, 50)" % random.randint(60, 100)
+                return "hsl(%d%, 100%%, 50)" % random.randint(60, 100)
 
             tokenizer = RegexpTokenizer(r'\w+')
             for sentiment in ['positive', 'neutral', 'negative']:
